162d8ee88139f8300d8fed5f82a61909669c3348
Alireza Talakoobi <alirezatalakoobi@gmail.com>
2021-06-03 14:29:24 +0200

    some change to discriminator and new train(v_1)

--- model/discriminator.py
+++ model/discriminator.py
@@ -3,11 +3,11 @@ from torch import nn
 
 #model based on paper 6 and https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 class Discriminator(nn.Module):
-    def __init__(self, in_channels, out_channel):
+    def __init__(self, num_classes):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
             # layer 1
-            nn.Conv2d(in_channels, 64, (4*4), 2, bias=False),
+            nn.Conv2d(num_classes, 64, (4*4), 2, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # layer 2
             nn.Conv2d(64, 128, (4*4), 2, bias=False),
@@ -20,7 +20,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # layer 5
             nn.Conv2d(512, 1, (4*4), 2, bias=False),
-            nn.Upsample(scale_factor=out_channel, mode='bilinear') #https://pytorch.org/docs/stable/generated/torch.nn.Upsample.html
+            nn.Upsample(scale_factor=32, mode='bilinear') #https://pytorch.org/docs/stable/generated/torch.nn.Upsample.html
         )
 
     def forward(self, input):
---
+++ train_v2.py
@@ -0,0 +1,370 @@
+import argparse
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from dataset.CamVid import CamVid
+from dataset.IDDA import IDDA
+import os
+from model.build_BiSeNet import BiSeNet
+from moel.discriminator import discriminator
+import torch
+from tensorboardX import SummaryWriter
+import tqdm
+import numpy as np
+from utils import poly_lr_scheduler
+from utils import reverse_one_hot, compute_global_accuracy, fast_hist, \
+    per_class_iu
+from loss import DiceLoss
+
+from torch.cuda import amp
+
+scaler = amp.GradScaler()
+
+
+# clear the cache to train ResnNet 101 
+
+# torch.cuda.empty_cache()
+
+def val(args, model, dataloader):
+    print('start val!')
+    # label_info = get_label_info(csv_path)
+    with torch.no_grad():
+        model.eval()
+        precision_record = []
+        hist = np.zeros((args.num_classes, args.num_classes))
+        for i, (data, label) in enumerate(dataloader):
+            if torch.cuda.is_available() and args.use_gpu:
+                data = data.cuda()
+                label = label.cuda()
+
+            # get RGB predict image
+            predict = model(data).squeeze()
+            predict = reverse_one_hot(predict)
+            predict = np.array(predict.cpu())
+
+            # get RGB label image
+            label = label.squeeze()
+            if args.loss == 'dice':
+                label = reverse_one_hot(label)
+            label = np.array(label.cpu())
+
+            # compute per pixel accuracy
+
+            precision = compute_global_accuracy(predict, label)
+            hist += fast_hist(label.flatten(), predict.flatten(), args.num_classes)
+
+            # there is no need to transform the one-hot array to visual RGB array
+            # predict = colour_code_segmentation(np.array(predict), label_info)
+            # label = colour_code_segmentation(np.array(label), label_info)
+            precision_record.append(precision)
+        precision = np.mean(precision_record)
+        # miou = np.mean(per_class_iu(hist))
+        miou_list = per_class_iu(hist)[:-1]
+        # miou_dict, miou = cal_miou(miou_list, csv_path)
+        miou = np.mean(miou_list)
+        print('precision per pixel for test: %.3f' % precision)
+        print('mIoU for validation: %.3f' % miou)
+        # miou_str = ''
+        # for key in miou_dict:
+        #     miou_str += '{}:{},\n'.format(key, miou_dict[key])
+        # print('mIoU for each class:')
+        # print(miou_str)
+        return precision, miou
+
+
+def train(args, model_G, model_D, optimizer_G, optimizer_D, dataloader_train, dataloader_val, IDDA_dataloader, curr_epoch):
+    writer = SummaryWriter(comment=''.format(args.optimizer_G, args.context_path))#not important for now
+    if args.loss_G == 'dice':
+        loss_func_G = DiceLoss()
+    elif args.loss_G == 'crossentropy':
+        loss_func_G = torch.nn.CrossEntropyLoss()
+        
+    if args.loss_adv == 'BCEwithlogits':
+        loss_func_adv = torch.nn.BCEWithLogitsLoss()
+        
+    if args.loss_D == 'BCEwithlogits':
+        loss_func_D = torch.nn.BCEWithLogitsLoss()
+        
+    max_miou = 0
+    step = 0
+    for epoch in range(curr_epoch + 1, args.num_epochs + 1):  # added +1 shift to finish with an eval
+        lr_G = poly_lr_scheduler(optimizer_G, args.learning_rate, iter=epoch, max_iter=args.num_epochs) #not important for now
+        lr_D = poly_lr_scheduler(optimizer_D, args.learning_rate, iter=epoch, max_iter=args.num_epochs)#not important for now
+        model_G.train()
+        model_D.train()
+        tq = tqdm.tqdm(total=len(dataloader_train) * args.batch_size) #not important for now
+        tq.set_description('epoch %d, lr %f' % (epoch, lr)) # not important for now
+
+        ## set the ground truth for the discriminator
+        source_label = 0
+        target_label = 1
+        # iniate lists to track the losses 
+        loss_G_record = []
+        loss_adv_record = []  ## we added a new list to track the advarsirial loss
+        loss_D_record = []
+        
+        source_train_loader = enumerate(dataloader_train)
+        s_size = len(list(source_train_loader))
+        target_loader = enumarate(IDDA_dataloader)
+        t_size = len(list(target_loader))
+
+        for i in range(t_size):
+            if(i % s_size == 0):
+                source_train_loader = enumerate(dataloader_train)
+
+        #train G:
+        
+            for param in model_D.parameters():
+                param.requires_grad = False
+
+            #train with source:
+
+            _, batch = source_train_loader.next()
+            data, label = batch
+
+            with amp.autocast():
+                if torch.cuda.is_available() and args.use_gpu:
+                    data = data.cuda()
+                    label = label.cuda()
+                output_s, output_sup1, output_sup2 = model_G(data)
+                loss1 = loss_func_G(output_s, label)
+                loss2 = loss_func_G(output_sup1, label)
+                loss3 = loss_func_G(output_sup2, label)
+                loss_G = loss1 + loss2 + loss3
+
+            scaler.scale(loss_G).backward()
+
+            #train with target:
+
+            _, batch = target_loader.next()
+            data, label, = batch
+
+            with amp.autocast():
+                if torch.cuda.is_available() and args.use_gpu:
+                    data = data.cuda()
+                output_t, output_sup1, output_sup2 = model_G(data)
+                D_out = model_D(F.softmax(output_t))
+                loss_adv = loss_func_adv(D_out , Variable(torch.FloatTensor(D_out.data.size()).fill_(source_label)).cuda() )
+
+            scaler.scale(loss_adv).backward()
+
+        # train D:
+            for param in model_D.parameters():
+                param.requires_grad = True
+
+            #train with source:
+
+            output_s = output_s.detach()
+            with amp.autocast():
+                D_out = model_D(F.softmax(output_s))  ## we feed the discriminator with the output of the model
+                loss_D = loss_func_D(D_out, Variable(torch.FloatTensor(D_out.data.size()).fill_(target_label)).cuda())  ## add the adversarial loss
+            scaler.scale(loss_D).backward()
+
+            #train with target:
+
+            output_t = output_t.detach()
+            with amp.autocast():
+                D_out = model_D(F.softmax(output_t))  ## we feed the discriminator with the output of the model
+                loss_D = loss_func_D(D_out, Variable(
+                    torch.FloatTensor(D_out.data.size()).fill_(target_label)).cuda())  ## add the adversarial loss
+            scaler.scale(loss_D).backward()
+
+            tq.update(args.batch_size)
+            tq.set_postfix(loss_seg='%.6f' % loss_seg)
+            tq.set_postfix(loss_adv='%.6f' % loss_adv)
+            tq.set_postfix(loss_D='%.6f' % loss_adv_D)
+
+            optimizer_G.zero_grad()
+            optimizer_D.zero_grad()
+
+            scaler.step(optimizer_G)
+            scaler.step(optimizer_D)  # optimizer for discriminator
+
+            scaler.update()
+            
+            
+        step += 1
+        writer.add_scalar('loss_G_step', loss_G, step)  ## track the segmentation loss 
+        writer.add_scalar('loss_adv_step', loss_adv, step)  ## track the adversarial loss 
+        writer.add_scalar('loss_D_step', loss_D, step)  ## track the adversarial loss 
+
+        loss_G_record.append(loss_G.item())
+        loss_adv_record.append(loss_adv.item())
+        loss_D_record.append(loss_D.item())
+
+        tq.close()
+        loss_G_train_mean = np.mean(loss_G_record)
+        loss_adv_train_mean = np.mean(loss_adv_record)
+        loss_D_train_mean = np.mean(loss_D_record)
+        writer.add_scalar('epoch/loss_G_train_mean', float(loss_G_train_mean), epoch)
+        writer.add_scalar('epoch/loss_adv_train_mean', float(loss_adv_train_mean), epoch)
+        writer.add_scalar('epoch/loss_D_train_mean', float(loss_D_train_mean), epoch)
+    
+        
+        
+        
+        #the checkpoint needs rewriting
+        """"
+        loss_train_mean = np.mean(loss_record)
+        writer.add_scalar('epoch/loss_epoch_train', float(loss_train_mean), epoch)
+        print('loss for train : %f' % (loss_train_mean))
+        if epoch % args.checkpoint_step == 0 and epoch != 0:
+            if not os.path.isdir(args.save_model_path):
+                os.mkdir(args.save_model_path)
+            state = {
+                "epoch": epoch,
+                "model_state": model.module.state_dict(),
+                "optimizer": optimizer.state_dict()
+            }
+            torch.save(state, os.path.join(args.save_model_path, 'latest_dice_loss.pth'))
+            print("*** epoch " + str(epoch) + " saved as recent checkpoint!!!")
+
+        if epoch % args.validation_step == 0 and epoch != 0:
+            precision, miou = val(args, model, dataloader_val)
+            if miou > max_miou:
+                max_miou = miou
+                os.makedirs(args.save_model_path, exist_ok=True)
+                state = {
+                    "epoch": epoch,
+                    "model_state": model.module.state_dict(),
+                    "optimizer": optimizer.state_dict()
+                }
+                torch.save(state, os.path.join(args.save_model_path, 'best_dice_loss.pth'))
+                print("*** epoch " + str(epoch) + " saved as best checkpoint!!!")
+            writer.add_scalar('epoch/precision_val', precision, epoch)
+            writer.add_scalar('epoch/miou val', miou, epoch)
+            """
+
+def main(params):
+    # basic parameters
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=300, help='Number of epochs to train for')
+    parser.add_argument('--epoch_start_i', type=int, default=0, help='Start counting epochs from this number')
+    parser.add_argument('--checkpoint_step', type=int, default=10, help='How often to save checkpoints (epochs)')
+    parser.add_argument('--validation_step', type=int, default=10, help='How often to perform validation (epochs)')
+    parser.add_argument('--dataset', type=str, default="CamVid", help='Dataset you are using.')
+    parser.add_argument('--crop_height', type=int, default=720, help='Height of cropped/resized input image to network')
+    parser.add_argument('--crop_width', type=int, default=960, help='Width of cropped/resized input image to network')
+    parser.add_argument('--batch_size', type=int, default=1, help='Number of images in each batch')
+    parser.add_argument('--context_path', type=str, default="resnet101",
+                        help='The context path model you are using, resnet18, resnet101.')
+    parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate used for train')
+    parser.add_argument('--data_CamVid', type=str, default='', help='path of training data_CamVid')
+    parser.add_argument('--data_IDDA', type=str, default='', help='path of training data_IDDA')
+    parser.add_argument('--num_workers', type=int, default=4, help='num of workers')
+    parser.add_argument('--num_classes', type=int, default=32, help='num of object classes (with void)')
+    parser.add_argument('--cuda', type=str, default='0', help='GPU ids used for training')
+    parser.add_argument('--use_gpu', type=bool, default=True, help='whether to user gpu for training')
+    parser.add_argument('--pretrained_model_path', type=str, default=None, help='path to pretrained model')
+    parser.add_argument('--save_model_path', type=str, default=None, help='path to save model')
+    parser.add_argument('--optimizer_G', type=str, default='rmsprop', help='optimizer, support rmsprop, sgd, adam')
+    parser.add_argument('--optimizer_D', type=str, default='rmsprop', help='optimizer, support rmsprop, sgd, adam')
+    parser.add_argument('--loss', type=str, default='dice', help='loss function, dice or crossentropy')
+
+    args = parser.parse_args(params)
+
+    # create dataset and dataloader for CamVid
+    CamVid_train_path = [os.path.join(args.data_CamVid, 'train'), os.path.join(args.data_CamVid, 'val')]
+    CamVid_train_label_path = [os.path.join(args.data_CamVid, 'train_labels'),
+                               os.path.join(args.data_CamVid, 'val_labels')]
+    CamVid_test_path = os.path.join(args.data_CamVid, 'test')
+    CamVid_test_label_path = os.path.join(args.data_CamVid, 'test_labels')
+    CamVid_csv_path = os.path.join(args.data_CamVid, 'class_dict.csv')
+    CamVid_dataset_train = CamVid(CamVid_train_path, CamVid_train_label_path, CamVid_csv_path,
+                                  scale=(args.crop_height, args.crop_width),
+                                  loss=args.loss, mode='train')
+    CamVid_dataloader_train = DataLoader(
+        CamVid_dataset_train,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers,
+        drop_last=True
+    )
+    CamVid_dataset_val = CamVid(CamVid_test_path, CamVid_test_label_path, CamVid_csv_path,
+                                scale=(args.crop_height, args.crop_width),
+                                loss=args.loss, mode='test')
+    CamVid_dataloader_val = DataLoader(
+        CamVid_dataset_val,
+        # this has to be 1
+        batch_size=1,
+        shuffle=True,
+        num_workers=args.num_workers
+    )
+
+    # create dataset and dataloader for IDDA
+    IDDA_path = [os.path.join(args.data_IDDA, 'rgb')]
+    IDDA_label_path = [os.path.join(args.data_IDDA, 'labels')]
+    IDDA_csv_path = os.path.join(args.data_IDDA, 'classes_info.json')
+    IDDA_dataset = IDDA(IDDA_path, IDDA_label_path, IDDA_csv_path, scale=(args.crop_height, args.crop_width),
+                        loss=args.loss, mode='train')
+    IDDA_dataloader = DataLoader(
+        IDDA_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers,
+        drop_last=True
+    )
+
+    # build model_G
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda
+    model_G = BiSeNet(args.num_classes, args.context_path)
+    if torch.cuda.is_available() and args.use_gpu:
+        model_G = torch.nn.DataParallel(model_G).cuda()
+        
+    #build model_D
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda
+    model_D = discriminaor(args.num_classes)
+    if torch.cuda.is_available() and args.use_gpu:
+        model_D = torch.nn.DataParallel(model_D).cuda()
+
+    # build optimizer
+    if args.optimizer == 'rmsprop':
+        optimizer_G = torch.optim.RMSprop(model_G.parameters(), args.learning_rate)
+        optimize_D = torch.optim.RMSprop(model_D.parameters(), args.learning_rate)
+    elif args.optimizer == 'sgd':
+        optimizer_G = torch.optim.SGD(model_G.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4)
+        optimizer_D = torch.optim.SGD(model_D.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4)
+    elif args.optimizer == 'adam':
+        optimizer_G = torch.optim.Adam(model_G.parameters(), args.learning_rate)
+        ptimizer_D = torch.optim.Adam(model_D.parameters(), args.learning_rate)
+    else:  # rmsprop
+        print('not supported optimizer \n')
+        return None
+
+    curr_epoch = 0
+    # load pretrained model if exists
+    if args.pretrained_model_path is not None:
+        print('load model from %s ...' % args.pretrained_model_path)
+        state = torch.load(os.path.realpath(args.pretrained_model_path))
+        model.module.load_state_dict(state['model_state'])
+        optimizer.load_state_dict(state['optimizer'])
+        curr_epoch = state["epoch"] + 1
+        print(str(curr_epoch - 1) + " already trained")
+        print("start training from epoch " + str(curr_epoch))
+        print('Done!')
+
+    # train
+    train(args, model_G, model_D, optimizer_G, optimizer_D, CamVid_dataloader_train, CamVid_dataloader_val, IDDA_dataloader, curr_epoch)
+
+    # val(args, model, dataloader_val, csv_path)
+
+
+if __name__ == '__main__':
+    params = [
+        '--num_epochs', '100',
+        '--learning_rate', '2.5e-2',
+        '--data_CamVid', './CamVid',
+        '--data_IDDA', './IDDA',
+        '--num_workers', '8',
+        '--num_classes', '12',
+        '--cuda', '0',
+        '--batch_size', '8',
+        '--save_model_path', './checkpoints_101_sgd',
+        '--context_path', 'resnet101',  # set resnet18 or resnet101, only support resnet18 and resnet101
+        '--optimizer', 'sgd',
+        # '--pretrained_model_path', './checkpoints_101_sgd/latest_dice_loss.pth',
+        '--checkpoint_step', '5'
+
+    ]
+    main(params)
+
